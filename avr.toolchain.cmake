12eb5bb6c72696f079da9d4102d788c31650f27c
Vladyslav <rehanvladyslav@gmail.com>
2025-06-15 15:01:24 +0300

    Make cmake toolchain more verbose

--- avr.toolchain.cmake
+++ avr.toolchain.cmake
@@ -1,12 +1,25 @@
-set(AVRCPP   avr-g++)
-set(AVRC     avr-gcc)
-set(AVRSTRIP avr-strip)
-set(OBJCOPY  avr-objcopy)
-set(OBJDUMP  avr-objdump)
-set(AVRSIZE  avr-size)
-set(AVRDUDE  avrdude)
+set(CMAKE_SYSTEM_NAME      Generic)
+set(CMAKE_SYSTEM_PROCESSOR avr)
 
-set(CMAKE_SYSTEM_NAME  Generic)
+find_program(AVRCPP   avr-g++     REQUIRED)
+find_program(AVRC     avr-gcc	    REQUIRED)
+find_program(AVRSTRIP avr-strip		REQUIRED)
+find_program(OBJCOPY  avr-objcopy REQUIRED)
+find_program(OBJDUMP  avr-objdump REQUIRED)
+find_program(AVRSIZE  avr-size    REQUIRED)
+find_program(AVRDUDE  avrdude     REQUIRED)
+
 set(CMAKE_CXX_COMPILER ${AVRCPP})
 set(CMAKE_C_COMPILER   ${AVRC})
 set(CMAKE_ASM_COMPILER ${AVRC})
+
+execute_process(
+	COMMAND ${AVRCPP} -print-search-dirs
+	OUTPUT_VARIABLE AVR_SEARCH_DIRS
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+string(REGEX MATCH "install: (.+)" AVR_INSTALL_DIR "${AVR_SEARCH_DIRS}")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+set(CMAKE_FIND_ROOT_PATH ${AVR_INSTALL_DIR})
